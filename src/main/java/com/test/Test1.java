f01c51d9cbfa1a5b3b6df78391fe0f72fb209cb3
erdemt <erdemtopdas@gmail.com>
2017-08-06 18:21:30 +0300

    2

--- src/main/java/com/test/Test1.java
+++ src/main/java/com/test/Test1.java
@@ -4,7 +4,6 @@ public class Test1 {
 
 	
 	public static void main( String[] args ) {
-		System.out.println( "hello git" );
-		System.out.println( "hello topic11111111111" );
+		System.out.println( "2" );
 	}
 }
